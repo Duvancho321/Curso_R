ff8dfd2bae43c6d723e50aa21879a2f795a2281b
duvancho321 <duvancho321@gmail.com>
2020-06-14 00:35:50 -0500

    Update_13/06

---
+++ R/CursoR.R
@@ -0,0 +1,10 @@
+#' Un Curso Amigable Para R
+#'
+#' Contiene un Curso amigable para R enfocado especialmente en el tidyverse
+#'
+#' @docType package
+#'
+#' @author Leonardo Lopez \email{jollopezma@unal.edu.co} & Duvan Nieves \email{dnieves@unal.edu.co}
+#'
+#' @name CursoR
+NULL
