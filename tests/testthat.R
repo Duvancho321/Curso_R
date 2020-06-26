10bf532b58bd9dc54745a5939b3746f082b01687
Duvancho321 <duvancho321@gmail.com>
2020-06-26 01:21:34 -0500

    pkg_update_25/06

---
+++ tests/testthat.R
@@ -0,0 +1,5 @@
+Sys.setenv("R_TESTS" = "")
+library(testthat)
+library(CursoR)
+
+test_check("CursoR")
---
+++ tests/testthat/Test_1.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(CursoR)
+learnr::run_tutorial("Curso_R1", package = "CursoR")
+test_check("CursoR")
